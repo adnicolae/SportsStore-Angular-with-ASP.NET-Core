e13a848982a4268b4cef8e84cce06dc65944fec4
Andrei Nicolae <u1516780@live.warwick.ac.uk>
2017-10-24 13:29:26 +0100

    Added GET and POST for Supplier.

---
+++ Controllers/SupplierValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.BindingTargets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    [Route("api/suppliers")]
+    public class SupplierValuesController : Controller
+    {
+        private DataContext _context;
+
+        public SupplierValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Supplier> GetSuppliers()
+        {
+            return _context.Suppliers;
+        }
+
+        [HttpPost]
+        public IActionResult CreateSupplier([FromBody] SupplierData supplierData)
+        {
+            if (ModelState.IsValid)
+            {
+                Supplier s = supplierData.Supplier;
+                _context.Add(s);
+                _context.SaveChanges();
+                return Ok(s.SupplierId);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
